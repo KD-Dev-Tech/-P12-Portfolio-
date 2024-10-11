2b78a2fab709dadca2705a780fc8782ae7c6c2d3
KD-Dev-Tech <08.kdlan@gmail.com>
2024-10-11 13:02:12 +0200

    Ajout animation & modification responsive

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,6 +2,7 @@ import { Button } from "react-bootstrap";
 import { FaDownload } from "react-icons/fa";
 import profil from "../../public/assets/testimg.jpg";
 import aboutData from "../data/about.json";
+import '../styles/home.css'
 
 
 
@@ -11,11 +12,24 @@ function AboutMe() {
     return (
         <div>
             <section id="about">
+                <div className="container_animation">
+                    <h1>Hello, I&apos;m</h1>
+                    <div className="animation">
+                        <div className="first">
+                            <div style={{display:'flex', alignItems:'self-end'}}>{about.firstname} {about.lastname}</div>
+                        </div>
+                        <div className="second">
+                            <div>{about.title}</div>
+                        </div>
+                    </div>
+                </div>
+                {/* <h1>Hello, I&apos;m {about.firstname} {about.lastname}</h1> */}
                 <img className="profile_image" src={profil} alt="Photo de profil" />
                 <div className="about_me">
-                    <h1>{about.firstname} {about.lastname}</h1>
-                    <h2>{about.title}</h2>
+                    
+        
                     <p>{about.description}</p>
+                    <p>{about.contact}</p>
                     <div className="about_btn">
                         <a href="../assets/CV.pdf"target="_blank"rel="noopener noreferrer">
                             <Button style={{borderRadius:'25px'}} bg="dark" variant="info">
