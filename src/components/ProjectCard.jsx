6157e0675364291a4e43bb92182813bd26e8968b
KD-Dev-Tech <08.kdlan@gmail.com>
2024-10-15 13:50:48 +0200

    modification design ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,18 +4,17 @@ import { FaGithub } from 'react-icons/fa';
 
 function projectCard ({project}) {
     return (
-        <Card className="project-card">
-            
+        <Card className="project-card"> 
             <Card.Img variant="top" src={project.image} alt={project.name} />
             <Card.Body>
-                <Card.Title>{project.name}</Card.Title>
+                <Card.Title style={{fontWeight:'bold'}}>{project.name}</Card.Title>
                 <div className="project-card__technologies">
                     {project.technologies.map((tech, index) => (
                         <span key={index} className="project-card__technology">{tech}</span>
                     ))}
                 </div>
                 <Button variant="primary" href={project.github} target="_blank" rel="noopener noreferrer">
-                    <FaGithub style={{ marginRight: '8px' }} />
+                    <FaGithub style={{ marginRight: '8px', backgroundColor:'black',borderRadius:'25px' }} />
                     Voir sur GitHub
                 </Button>
             </Card.Body>
